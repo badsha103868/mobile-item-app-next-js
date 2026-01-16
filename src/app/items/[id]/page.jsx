06a7e7980e50d188f1de5b7582888f1780f0379d
badsha103868 <badshagolder5@gmail.com>
2026-01-16 17:42:36 +0600

    added item card next image tag use and udate styling

--- src/app/items/[id]/page.jsx
+++ src/app/items/[id]/page.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 import items from "@/data/items.json";
-const ItemDetails =async ({ params }) => {
-  const { id }  = await params
-   const item = items.find((i)=> i.id === id)
-   if(!item){
-    return  <p className="p-6 text-center">Item not found</p>;
-   }
-    
+import Image from "next/image";
+
+const ItemDetails = async({ params }) => {
+  const { id } = await params;
+  const item = items.find((i) => i.id === id);
+
+  if (!item) {
+    return <p className="p-6 text-center">Item not found</p>;
+  }
+
   return (
-   <div className="max-w-3xl mx-auto p-6">
-      <img
-        src={item.image}
-        alt={item.name}
-        className="rounded mb-6 w-full h-64 object-cover"
-      />
+    <div className="max-w-3xl mx-auto p-6 bg-white rounded shadow">
+      <div className="relative w-full h-64 mb-6 overflow-hidden rounded-lg">
+        <Image
+          src={item.image}
+          alt={item.name}
+          fill
+          className="object-cover"
+        />
+      </div>
 
       <h1 className="text-3xl font-bold mb-2">{item.name}</h1>
       <p className="text-gray-500 mb-4">{item.category}</p>
       <p className="mb-4 font-semibold">{item.price}</p>
-      <p className="mb-6">{item.description}</p>
+
+      <p className="mb-6 text-gray-700">{item.description}</p>
     </div>
   );
 };
--- src/components/Card/ItemCard.jsx
+++ src/components/Card/ItemCard.jsx
@@ -1,21 +1,24 @@
+import Image from "next/image";
 import Link from "next/link";
-
-import React from 'react';
+import React from "react";
 
 const ItemCard = ({ item }) => {
   return (
-     <div className="border rounded-lg shadow hover:shadow-lg transition p-4 flex flex-col">
-      <img
-        src={item.image}
-        alt={item.name}
-        className="rounded mb-3 h-40 object-cover"
-      />
+    <div className="border rounded-lg shadow hover:shadow-lg transition p-4 flex flex-col bg-white">
+      <div className="relative w-full h-52 mb-4 overflow-hidden rounded">
+        <Image
+          src={item.image}
+          alt={item.name}
+          fill
+          className="object-cover"
+        />
+      </div>
 
       <h2 className="text-xl font-bold mb-1">{item.name}</h2>
-      <p className="text-sm text-gray-500">{item.category}</p>
-      <p className="mt-2 font-semibold">{item.price}</p>
+      <p className="text-sm text-gray-500 mb-2">{item.category}</p>
+      <p className="font-semibold mb-2">{item.price}</p>
 
-      <p className="mt-2 text-sm line-clamp-3">{item.description}</p>
+      <p className="mt-2 text-sm line-clamp-3 text-gray-700">{item.description}</p>
 
       <Link
         href={`/items/${item.id}`}
